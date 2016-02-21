c86a255e8eb0c92bdc564921bb5540354aa64c64
rachid1805 <raannn@hotmail.com>
2016-02-21 14:15:39 -0500

    Rename PatientArrivalGenerator to PatientGenerator

---
+++ PatientGenerator/Program.cs
@@ -0,0 +1,30 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PatientGenerator
+{
+  class Program
+  {
+    static void Main(string[] args)
+    {
+      // If a number of patient to generate in a period of time are not specified, exit program.
+      if (args.Length != 3)
+      {
+        // Display the proper way to call the program.
+        Console.WriteLine("Usage: PatientArrivalGenerator.exe directory numberOfPatientsToGenerate periodOfTimeMs");
+        return;
+      }
+
+      string pathFile = args[0];
+      int numberOfPatientsToGenerate = int.Parse(args[1]);
+      int periodOfTimeMs = int.Parse(args[2]);
+
+      // Creates or overwrites the specified file
+      File.Create(pathFile);
+    }
+  }
+}
--- PatientGenerator/Properties/AssemblyInfo.cs
+++ PatientGenerator/Properties/AssemblyInfo.cs
@@ -5,11 +5,11 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("PatientArrivalGenerator")]
+[assembly: AssemblyTitle("PatientGenerator")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("PatientArrivalGenerator")]
+[assembly: AssemblyProduct("PatientGenerator")]
 [assembly: AssemblyCopyright("Copyright ©  2016")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
