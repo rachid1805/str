51bcb83fbc61b3906d123173c800c0b8827e6cc4
JS <arekhyne@hotmail.com>
2016-03-06 21:26:27 -0500

    SqlConnection lifetime now handled by using. Param "afterEventId" is now an unsigned int.

--- Common/Helpers/MedWatchDAL.cs
+++ Common/Helpers/MedWatchDAL.cs
@@ -12,51 +12,51 @@ namespace Common.Helpers
 
         public static void InsertBulkHospitalEvents(IEnumerable<IHospitalEvent> events)
         {
-            try
+            using (SqlConnection connection = new SqlConnection(ConnectionString))
             {
-                using (SqlConnection connection = new SqlConnection(ConnectionString))
+                try
                 {
                     connection.Open();
 
-                    using (var bulkCopy = new SqlBulkCopy(ConnectionString))
+                    using ( var bulkCopy = new SqlBulkCopy( ConnectionString ) )
                     {
                         // Create the data table
                         DataTable table = new DataTable();
-                        table.Columns.Add("Id", typeof(int));
-                        table.Columns.Add("HospitalId", typeof(int));
-                        table.Columns.Add("PatientId", typeof(int));
-                        table.Columns.Add("EventType", typeof(short));
-                        table.Columns.Add("EventTimestamp", typeof(DateTime));
-                        table.Columns.Add("DiseaseType", typeof(short));
-                        table.Columns.Add("DoctorId", typeof(int));
-                        foreach (var e in events)
+                        table.Columns.Add( "Id", typeof( int ) );
+                        table.Columns.Add( "HospitalId", typeof( int ) );
+                        table.Columns.Add( "PatientId", typeof( int ) );
+                        table.Columns.Add( "EventType", typeof( short ) );
+                        table.Columns.Add( "EventTimestamp", typeof( DateTime ) );
+                        table.Columns.Add( "DiseaseType", typeof( short ) );
+                        table.Columns.Add( "DoctorId", typeof( int ) );
+                        foreach ( var e in events )
                         {
-                            table.Rows.Add(e.EventId,
-                                           e.HospitalId,
-                                           e.PatiendId,
-                                           Convert.ToInt16(e.EventType),
-                                           e.EventTime,
-                                           e.DiseaseType,
-                                           e.DoctorId);
+                            table.Rows.Add( e.EventId,
+                                            e.HospitalId,
+                                            e.PatiendId,
+                                            Convert.ToInt16( e.EventType ),
+                                            e.EventTime,
+                                            e.DiseaseType,
+                                            e.DoctorId );
                         }
 
                         // Specify the destination table name in the database
                         bulkCopy.DestinationTableName = "HospitalEvent";
 
                         // Store in the database
-                        bulkCopy.WriteToServer(table);
+                        bulkCopy.WriteToServer( table );
                     }
+
+                    connection.Close();
                 }
-            }
-            catch (Exception ex)
-            {
-                Console.WriteLine(ex);
-            }
-            finally
-            {
+                catch ( Exception ex )
+                {
+                    Console.WriteLine( ex );
+                }
             }
         }
 
+        // TODO remove this
         public static void InsertHospitalEvents( IEnumerable<IHospitalEvent> events )
         {
             SqlConnection conn = null;
@@ -129,61 +129,59 @@ namespace Common.Helpers
             }
         }
 
-        public static IEnumerable<IHospitalEvent> FindHospitalEventsAfter( int hospitalId, int afterEventId )
+        public static IEnumerable<IHospitalEvent> FindHospitalEventsAfter( int hospitalId, uint afterEventId )
         {
             var events = new List<IHospitalEvent>();
-
-            SqlConnection conn = null;
-
+            
             try
             {
-                conn = new SqlConnection( ConnectionString );
-                conn.Open();
-
-                using ( var cmd = new SqlCommand( "HospitalEventSelectCommand", conn ) )
+                using ( var conn = new SqlConnection( ConnectionString ) )
                 {
-                    cmd.CommandType = System.Data.CommandType.StoredProcedure;
+                    conn.Open();
 
-                    var param = new SqlParameter();
-                    param.ParameterName = "@HospitalId";
-                    param.Value = hospitalId;
-                    param.SqlDbType = System.Data.SqlDbType.Int;
-                    cmd.Parameters.Add( param );
-
-                    param = new SqlParameter();
-                    param.ParameterName = "@AfterEventId";
-                    param.Value = afterEventId;
-                    param.SqlDbType = System.Data.SqlDbType.Int;
-                    cmd.Parameters.Add( param );
-
-                    SqlDataReader dr = cmd.ExecuteReader();
-
-                    while ( dr.Read() )
+                    using ( var cmd = new SqlCommand( "HospitalEventSelectCommand", conn ) )
                     {
-                        var e = new HospitalEvent
-                        {
-                            EventId = dr.GetInt32( 0 ),
-                            HospitalId = hospitalId,
-                            PatiendId = dr.GetInt32( 1 ),
-                            EventType = (HospitalEventType) dr.GetInt16( 2 ),
-                            EventTime = dr.GetDateTime( 3 ),
-                            DiseaseType = dr.IsDBNull( 4 ) ? (DiseaseType?) null : (DiseaseType) dr.GetInt16( 4 ),
-                            DoctorId = dr.IsDBNull( 5 ) ? (int?) null : dr.GetInt32( 5 )
-                        };
+                        cmd.CommandType = System.Data.CommandType.StoredProcedure;
 
-                        events.Add( e );
+                        var param = new SqlParameter();
+                        param.ParameterName = "@HospitalId";
+                        param.Value = hospitalId;
+                        param.SqlDbType = System.Data.SqlDbType.Int;
+                        cmd.Parameters.Add( param );
+
+                        param = new SqlParameter();
+                        param.ParameterName = "@AfterEventId";
+                        param.Value = afterEventId;
+                        param.SqlDbType = System.Data.SqlDbType.Int;
+                        cmd.Parameters.Add( param );
+
+                        SqlDataReader dr = cmd.ExecuteReader();
+
+                        while ( dr.Read() )
+                        {
+                            var e = new HospitalEvent
+                            {
+                                EventId = dr.GetInt32( 0 ),
+                                HospitalId = hospitalId,
+                                PatiendId = dr.GetInt32( 1 ),
+                                EventType = (HospitalEventType) dr.GetInt16( 2 ),
+                                EventTime = dr.GetDateTime( 3 ),
+                                DiseaseType = dr.IsDBNull( 4 ) ? (DiseaseType?) null : (DiseaseType) dr.GetInt16( 4 ),
+                                DoctorId = dr.IsDBNull( 5 ) ? (int?) null : dr.GetInt32( 5 )
+                            };
+
+                            events.Add( e );
+                        }
                     }
+
+                    conn.Close();
                 }
             }
             catch ( Exception ex )
             {
                 Console.WriteLine(ex);
             }
-            finally
-            {
-                conn.Close();
-            }
-
+          
             return events;
         }
 
@@ -192,88 +190,85 @@ namespace Common.Helpers
             var hospitals = new List<Hospital>();
 
             string sql = "SELECT Id, Name, AssignedDoctors FROM Hospital ORDER BY Name";
-
-            SqlConnection conn = null;
-
+            
             try
             {
-                conn = new SqlConnection( ConnectionString );
-                conn.Open();
-
-                using ( var cmd = new SqlCommand( sql, conn ) )
+                using ( var conn = new SqlConnection( ConnectionString ) )
                 {
-                    cmd.CommandType = System.Data.CommandType.Text;
-                    
-                    SqlDataReader dr = cmd.ExecuteReader();
+                    conn.Open();
 
-                    while ( dr.Read() )
+                    using ( var cmd = new SqlCommand( sql, conn ) )
                     {
-                        var h = new Hospital
-                        {
-                            Id = dr.GetInt32( 0 ),
-                            Name = dr.GetString( 1 ),
-                            AssignedDoctors = dr.GetInt32(2)
-                        };
+                        cmd.CommandType = System.Data.CommandType.Text;
 
-                        hospitals.Add( h );
+                        SqlDataReader dr = cmd.ExecuteReader();
+
+                        while ( dr.Read() )
+                        {
+                            var h = new Hospital
+                            {
+                                Id = dr.GetInt32( 0 ),
+                                Name = dr.GetString( 1 ),
+                                AssignedDoctors = dr.GetInt32( 2 )
+                            };
+
+                            hospitals.Add( h );
+                        }
                     }
+
+                    conn.Close();
                 }
             }
             catch ( Exception ex )
             {
                 Console.WriteLine(ex);
             }
-            finally
-            {
-                conn.Close();
-            }
-
+          
             return hospitals;
         }
 
-        public static IEnumerable<Disease> FindDiseses()
+        public static IEnumerable<Disease> FindDiseases()
         {
             var diseases = new List<Disease>();
 
             string sql = "SELECT Id, Name, Priority, RequiredTime, TimeUnit FROM Disease";
-
-            SqlConnection conn = null;
-
+            
             try
             {
-                conn = new SqlConnection(ConnectionString);
-                conn.Open();
-
-                using (var cmd = new SqlCommand(sql, conn))
+                using ( var conn = new SqlConnection( ConnectionString ) )
                 {
-                    cmd.CommandType = System.Data.CommandType.Text;
+                    conn.Open();
 
-                    SqlDataReader dr = cmd.ExecuteReader();
-
-                    while (dr.Read())
+                    using ( var cmd = new SqlCommand( sql, conn ) )
                     {
-                        var h = new Disease
-                        {
-                            Id = (DiseaseType)dr.GetInt16(0),
-                            Name = dr.GetString(1),
-                            Priority = (DiseasePriority)dr.GetInt16(2),
-                            RequiredTime = dr.GetInt32(3),
-                            TimeUnit = (RequiredTimeUnit)dr.GetInt16(4)
-                        };
+                        cmd.CommandType = System.Data.CommandType.Text;
 
-                        diseases.Add(h);
+                        SqlDataReader dr = cmd.ExecuteReader();
+
+                        while ( dr.Read() )
+                        {
+                            var h = new Disease
+                            {
+                                Id = (DiseaseType) dr.GetInt16( 0 ),
+                                Name = dr.GetString( 1 ),
+                                Priority = (DiseasePriority) dr.GetInt16( 2 ),
+                                RequiredTime = dr.GetInt32( 3 ),
+                                TimeUnit = (RequiredTimeUnit) dr.GetInt16( 4 )
+                            };
+
+                            diseases.Add( h );
+                        }
                     }
+
+                    conn.Close();
                 }
+               
             }
             catch (Exception ex)
             {
                 Console.WriteLine(ex);
             }
-            finally
-            {
-                conn.Close();
-            }
-
+          
             return diseases;
         }
     }
